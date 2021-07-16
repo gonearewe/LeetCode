2640de3cd2e196932837ce8fc7e41ee042560bb9
Euan Mactavish <cristmactavish@outlook.com>
2021-07-16 23:29:23 +0800

    last time update

--- 03-无重复字符的最长子串.go
+++ 03-无重复字符的最长子串.go
@@ -1,10 +1,10 @@
 /*
-给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+给定一个字符串，请你找出其中不含有重复字符的最长子串的长度。
 
-示例 1:
+示例 1:
 
 输入: "abcabcbb"
-输出: 3 
+输出: 3
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
 示例 2:
 
@@ -15,13 +15,13 @@
 
 输入: "pwwkew"
 输出: 3
-解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 */
 func lengthOfLongestSubstring(s string) int {    //滑动窗口法
 max:=func (a int,b int) int{if a>b {return a} else {return b}}  //匿名函数
 	var m=map[rune]int{}    //用map加快查找
-	var start,maxlen int  
+	var start,maxlen int
 	for index,char:=range s{
 		j,ok:=m[char]
         if ok {
